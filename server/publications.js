ff138caf68206089d2aefd4d14b8bafbf4ea9b45
John Crisostomo <jd@snapzio.com>
2016-05-11 14:27:18 +0800

    added emoji support / challenge done

--- server/publications.js
+++ server/publications.js
@@ -5,3 +5,7 @@ Meteor.publish('users', function() {
 Meteor.publish('chats', function(userId) {
   return Chats.find({$or: [{user1Id: userId}, {user2Id: userId}]});
 });
+
+Meteor.publish('emojis', function() {
+  return Emojis.find();
+});
